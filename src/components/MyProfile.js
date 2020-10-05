73fc3c8bf86543ad567df079bfc5dadbfe6d1cda
CH <1925480119@qq.com>
2020-10-05 22:35:03 +0800

    [chen] submit form

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,50 +2,38 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      Input: 'You name',
-      Gender: "Male",
-      Description: "Description about yourself",
-      read: 'false'
-      
-    };
-    this.nameChange = this.nameChange.bind(this);
-    this.genderChange = this.genderChange.bind(this);
-    this.textarearChange = this.textarearChange.bind(this);
-    this.readChange = this.textarearChange.bind(this);
+  state = {
+    Input: 'You name',
+    Gender: "Male",
+    Description: "Description about yourself",
+    read: false    
+  };
 
-  }
-
-  nameChange(event){
-    this.setState({
-      // [event.target.Input]: event.target.value
+  nameChange=(event) => {
+    this.setState({     
      Input: event.target.value
     });
   }
 
-  genderChange(event){
+  genderChange=(event)=>{
     this.setState({
-      // [event.target.Gender]: event.target.value
       Gender: event.target.value
     });
   }
 
-  textarearChange(event){
+  textarearChange=(event)=>{
     this.setState({
-      // [event.target.Description]: event.target.value
      Description: event.target.value
     });
   }
 
-  handleSubmit(event){
+  handleSubmit=(event)=>{
     event.preventDefault();
-    console.log(JSON.stringify([event.target.state]));
+    console.log(JSON.stringify(this.state));
   }
-  readChange(event){
+  readChange=(event)=>{
     this.setState({
-      read: event.target.value
+      read: event.target.type === "checkbox"?event.target.checked:event.target.value
     })
   }
 
@@ -75,7 +63,7 @@ class MyProfile extends Component {
         </label>
 
         
-        <label style={{fontSize:'10px', with:'200px'}}><input  type='checkbox' value="" onChange={this.readChange}/>I have read the terms of conduct</label>
+        <label style={{fontSize:'10px', with:'200px'}}><input  type='checkbox' value= {this.state.read} onChange={this.readChange}/>I have read the terms of conduct</label>
       
 
         <input 
@@ -86,9 +74,8 @@ class MyProfile extends Component {
             || this.state.Input == 'You name'
             || !this.state.Description
             || this.state.Description == 'Description about yourself'
-            || !this.state.value == 'false'}  
+            || !this.state.read == true}  
           />  
-
       </form>
       </div>
     );
